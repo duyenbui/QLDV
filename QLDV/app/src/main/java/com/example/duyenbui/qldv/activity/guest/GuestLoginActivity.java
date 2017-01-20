11dd362f87bdaba34b36941e9d4eef27c4f1fe91
duyenbui <anhduyen1510@gmail.com>
2017-01-20 17:23:02 +0700

    UI Login

--- QLDV/app/src/main/java/com/example/duyenbui/qldv/activity/guest/GuestLoginActivity.java
+++ QLDV/app/src/main/java/com/example/duyenbui/qldv/activity/guest/GuestLoginActivity.java
@@ -1,15 +1,76 @@
 package com.example.duyenbui.qldv.activity.guest;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.duyenbui.qldv.R;
+import com.example.duyenbui.qldv.activity.MapActivity;
+
+import io.realm.Realm;
+import okhttp3.OkHttpClient;
 
 public class GuestLoginActivity extends AppCompatActivity {
+
+    private static final String HOST_NAME = "192.168.0.48:8081";
+
+    EditText txt_username;
+    EditText txt_password;
+    Button bt_login;
+    TextView bt_createAccount;
+
+    public Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_login);
+
+        txt_username = (EditText) findViewById(R.id.txt_name);
+        txt_password = (EditText) findViewById(R.id.txt_password);
+        bt_login = (Button) findViewById(R.id.bt_login);
+        bt_createAccount = (TextView) findViewById(R.id.bt_createAccount);
+
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GuestSignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void startAsyncTaskGetAPI(){
+        String url = Uri.parse(HOST_NAME).buildUpon().appendPath("user").build().toString();
+
+    }
+
+    public class AsyncTaskLoad extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... strings){
+            String url = strings[0];
+            OkHttpClient okHttpClient = new OkHttpClient();
+            return "Error can not get API";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
---
+++ QLDV/app/src/main/java/com/example/duyenbui/qldv/object/user.java
@@ -0,0 +1,80 @@
+package com.example.duyenbui.qldv.object;
+
+/**
+ * Created by code-engine-studio on 20/01/2017.
+ */
+
+public class user {
+
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String brithday;
+
+    public user() {
+    }
+
+    public user(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public user(String username, String password, String firstname, String lastname, String email, String brithday) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.brithday = brithday;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBrithday() {
+        return brithday;
+    }
+
+    public void setBrithday(String brithday) {
+        this.brithday = brithday;
+    }
+}
